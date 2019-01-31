1cc7fc70e20b00d7bb8e420e44f3397546aecd2b
Paul Shryock <paul@pshry.com>
2019-01-31 17:23:04 -0500

    Update webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
-const tailwindcss = require('tailwindcss'),
-			precss = require('precss'),
-			autoprefixer = require('autoprefixer');
-
 module.exports = {
-	plugins: [
-	  precss,
-    autoprefixer,
-    tailwindcss('./tailwind.js'),
-	]
+  plugins: [
+    require('autoprefixer'),
+    require('precss'),
+    require('tailwindcss')('./tailwind.js'),
+  ]
 }
